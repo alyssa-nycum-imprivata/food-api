b2a8ad0ef44eec84601bcc174f110d04aee02b12
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-01-21 15:57:38 -0600

    finished the getting other people's data part of the displaying foods practice

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,9 +1,14 @@
 const foodFactory = food => {
     return `
     <section class="foodList">
-    <h1 class="food">${food.name}</h1>
-    <p class="category">${food.category}</p>
-    <p class="ethnicity">${food.ethnicity}</p>
+        <div>
+            <h1 class="food">${food.name}</h1>
+            <p>${food.category}<br>${food.ethnicity}</p>
+            <p><strong>Ingredients:</strong> <br> ${food.ingredients}</p>
+            <p><strong>Country of Origin:</strong> <br> ${food.countries}</p>
+            <p><strong>Sugar per Serving:</strong> <br> ${food.sugar}</p>
+            <p><strong>Saturated Fat per Serving:</strong> <br> ${food.saturated_fat}</p>
+        </div>
     </section>
     `
 };
@@ -15,13 +20,54 @@ const addFoodToDom = foodHtml => {
 
 const url = "http://localhost:8088/food";
 
+// fetch(url)
+//     .then(foods => foods.json())
+//     .then(foodData => {
+//         foodData.forEach(food => {
+//             const foodHtml = foodFactory(food);
+//             addFoodToDom(foodHtml);
+//         });
+//         console.table(foodData)
+//     });
+
+
 fetch(url)
-    .then(foods => foods.json())
-    .then(foodData => {
-        foodData.forEach(food => {
-        const foodHtml = foodFactory(food);
-        addFoodToDom(foodHtml);
+    .then(resp => resp.json())
+    .then(myFoodData => {
+        myFoodData.forEach(food => {
+            console.log(food);
+
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(resp => resp.json())
+                .then(productInfo => {
+                    if (productInfo.product.ingredients_text) {
+                        food.ingredients = productInfo.product.ingredients_text
+                    } else {
+                        food.ingredients = "no ingredients listed"
+                    }
+                    if (productInfo.product.countries) {
+                        food.countries = productInfo.product.countries
+                    } else {
+                        food.countries = "no country of origin listed"
+                    }
+                    if (productInfo.product.nutriscore_data.sugars) {
+                        food.sugar = productInfo.product.nutriscore_data.sugars
+                    } else {
+                        food.sugar = "sugar not listed"
+                    }
+                    if (productInfo.product.nutriscore_data.saturated_fat) {
+                        food.saturated_fat = productInfo.product.nutriscore_data.saturated_fat
+                    } else {
+                        food.saturated_fat = "saturated fat not listed"
+                    }
+                    
+                    const foodHtml = foodFactory(food)
+
+                    addFoodToDom(foodHtml)
+                });
+
+        });
     });
-        console.table(foodData)
-    });
+
+
 
